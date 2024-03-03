a40eaf1e75e61191f61fd5c5111c3df933d1f4a8
Matheus Pereira Gonsalves <matheus.gonsalvespereira@gmail.com>
2024-03-03 09:02:41 -0300

    Exercicio 05, calculando total do pedido

---
+++ src/Exercicios_condicional/Ex05.java
@@ -0,0 +1,35 @@
+package Exercicios_condicional;
+import java.util.Scanner;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        //variaveis 
+        Scanner sc = new Scanner(System.in);
+        double codigo1 = 4f, codigo2 = 4.5, codigo3 = 5, codigo4 = 2, codigo5 = 1.5, total = 0;
+        int x, qtd;
+
+        //entrada do codigo
+        System.out.println("Digite o codigo do item desejado: ");
+        x = sc.nextInt();
+        System.out.println("Digite a quantidade desejada: ");
+        qtd = sc.nextInt();
+
+        //condicoes para calcular preco
+        if(x == 1){
+            total = (double) qtd * codigo1;
+        } else if (x == 2) {
+            total = (double) qtd * codigo2;
+        } else if (x == 3) {
+            total = (double) qtd * codigo3;
+        } else if (x == 4) {
+            total = (double) qtd * codigo4;
+        } else if (x == 5) {
+            total = (double) qtd * codigo5;
+        }
+
+        //mostrando total
+        System.out.printf("O total e: %.2f\n", total);
+
+        sc.close();
+    }
+}
